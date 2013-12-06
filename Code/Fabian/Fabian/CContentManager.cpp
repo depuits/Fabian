ce6a9a2d4ba3e9b67a5b12bdbf9f4093ed7b3de7
colmanjoeri <joeri.colman@student.howest.be>
2013-12-06 21:42:01 +0100

    added logoutput for mesh and texture loading

--- Code/Fabian/Fabian/CContentManager.cpp
+++ Code/Fabian/Fabian/CContentManager.cpp
@@ -45,6 +45,7 @@ IMesh *CContentManager::LoadMesh(const std::string& sFile)
 	if ( !IsMeshLoaded(sFile))
 	{
 		CLibrary lib;
+		CLog::Get()->Write(FLOG_LVL_INFO, FLOG_ID_APP, "Content: Loading new mesh using \"%s\"", "plugins/A3DLoader.fpm");
 		if( !lib.Load(/*"../debug/FPM_A3DLoader.dll") )*/"plugins/A3DLoader.fpm") )
 			return nullptr;
 		LOAD_MESHDATA fLoadMD = (LOAD_MESHDATA)lib.GetFunction("LoadData"); // meshData Loading function
@@ -56,12 +57,16 @@ IMesh *CContentManager::LoadMesh(const std::string& sFile)
 		// load from dll
 		MeshData *md = fLoadMD(sFile.c_str());
 
+		CLog::Get()->Write(FLOG_LVL_INFO, FLOG_ID_APP, "Content: Loaded MeshData ( v: %d - i: %d )", md->vCount / 8 /* divided by 8 becaus the vcount is x,y,z,nx,ny,nz,u,v*/, md->iCount);
 		IMesh *pMesh = m_pRenderer->LoadMesh(md);
 		// release dll data
 		fReleaseMD(md);
 
 		if( pMesh == nullptr )
+		{
+			CLog::Get()->Write(FLOG_LVL_ERROR, FLOG_ID_APP, "Content: Loading mesh failed");
 			return nullptr;
+		}
 		
 		m_mMeshMap[sFile] = pMesh;
 	}
@@ -74,6 +79,7 @@ IImage *CContentManager::LoadImage(const std::string& sFile)
 	if ( !IsImageLoaded(sFile))
 	{
 		CLibrary lib;
+		CLog::Get()->Write(FLOG_LVL_INFO, FLOG_ID_APP, "Content: Loading new Texture using \"%s\"", "plugins/SOILLoader.fpi");
 		if( !lib.Load("plugins/SOILLoader.fpi") )
 			return nullptr;
 		LOAD_IMAGEDATA fLoadID = (LOAD_IMAGEDATA)lib.GetFunction("LoadData"); // meshData Loading function
@@ -86,11 +92,15 @@ IImage *CContentManager::LoadImage(const std::string& sFile)
 		ImageData *id = fLoadID(sFile.c_str());
 
 		IImage *pImage = m_pRenderer->LoadImage(id);
+		CLog::Get()->Write(FLOG_LVL_INFO, FLOG_ID_APP, "Content: Loaded ImageData ( w: %d - h: %d )", id->w, id->h);
 		// release dll data
 		fReleaseID(id);
 
 		if( pImage == nullptr )
+		{
+			CLog::Get()->Write(FLOG_LVL_ERROR, FLOG_ID_APP, "Content: Loading texture failed");
 			return nullptr;
+		}
 		
 		m_mImageMap[sFile] = pImage;
 	}
